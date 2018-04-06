8a3cf46879fde313872c64af5c374e38a9c088da
thuitafaith <thuitamuthoni15@gmail.com>
2018-04-06 16:48:51 +0300

    reviews fuction created to display reviews

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from flask import render_template,request,redirect,url_for
 from app import app
 from .request import get_movies,get_movie,search_movie
+from .models import reviews
+from .forms import ReviewForm
+Review = reviews.Review
 
 #views
 @app.route('/')
@@ -29,7 +32,7 @@ def movie(movie_id):
     '''
     movie = get_movie(movie_id)
     title = f'{movie.title}'
-    return render_template('movie.html',id = movie_id, movie = movie)
+    return render_template('movie.html',id = movie_id, movie = movie,reviews = reviews)
 
 @app.route('/search/<movie_name>')
 def search(movie_name):
@@ -41,3 +44,17 @@ def search(movie_name):
     searched_movies = search_movie(movie_name_format)
     title = f'search results for {movie_name}'
     return render_template('search.html',movies = searched_movies)
+
+@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+    form = ReviewForm()
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(movie.id,title,movie.poster,review)
+        new_review.save_review()
+        return redirect(url_for('movie',id = movie.id ))
+    title = f'{movie.title} review'
+    return render_template('new_review.html',title = title, review_form=form,movie=movie)
